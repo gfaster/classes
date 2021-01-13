cce247ace590e248df795b29f3ad045d6fe6a7ae
Gavin Rohrer <gavin@rohrer.org>
2021-01-13 18:53:48 -0500

    Improved build script

--- build.bat
+++ build.bat
@@ -1,3 +1,21 @@
+python --version 2>NUL
+if errorlevel 1 goto errorNoPython
+pyinstaller -v>NUL
+if errorlevel 1 goto errorNoPyinstaller
+
 pyinstaller --onefile --icon=pixel_book.ico main.py
-copy config.json dist\config.json 
+copy config.json dist\config.json
 pause
+exit
+
+:errorNoPython
+echo.
+echo Error^: Python not installed
+pause
+exit
+
+:errorNoPyinstaller
+echo.
+echo Error^: Pyinstaller not installed
+pause
+exit
